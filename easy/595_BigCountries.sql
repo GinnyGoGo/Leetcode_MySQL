115be01a8b0969ed88100709f02e09db5159d9fe
GinnyGoGo <52679264+GinnyGoGo@users.noreply.github.com>
2020-04-02 08:39:19 -0400

    4-2

--- easy/595_BigCountries.sql
+++ easy/595_BigCountries.sql
@@ -6,6 +6,7 @@ FROM world
 WHERE area > 3000000 OR population > 25000000
 ;
 
+
 -- Approach II: WHERE & UNION (Faster)
 -- UNION can be a bit faster because each select can use it's own index, 
 -- while OR can use index for area and full-scan for population (or vise versa).
